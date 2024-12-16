f019181a345cef998111f9ccd6104922e469dd66
Joscha Feth <joscha@feth.com>
2024-12-16 23:44:34 +0000

    docs: leave TODO about Person class

--- affinity/model/__init__.py
+++ affinity/model/__init__.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel, model_serializer
 class MyBaseModel(BaseModel):
     @model_serializer(mode="wrap")
     def ser_model(self, nxt):
-        # TODO: improve this to possibky add the serializer only to models
-        # that have a `Person` class
+        # TODO: improve this to possibly make the serializer swap out any
+        # `PersonData` class with tis id, except in the `PersonValue` class
         if self.__class__.__qualname__ == "Attendee":
             return {
                 "emailAddress": self.emailAddress,
