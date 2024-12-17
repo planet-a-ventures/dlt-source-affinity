4fded9ea0ef960abb879d995715f4a8dfa39343f
Joscha Feth <joscha@feth.com>
2024-12-17 01:24:33 +0000

    fix: replace `PersonData` with its ID

--- affinity/model/__init__.py
+++ affinity/model/__init__.py
@@ -4,12 +4,7 @@ from pydantic import BaseModel, model_serializer
 class MyBaseModel(BaseModel):
     @model_serializer(mode="wrap")
     def ser_model(self, nxt):
-        # TODO: improve this to possibly make the serializer swap out any
-        # `PersonData` class with tis id, except in the `PersonValue` class
-        if self.__class__.__qualname__ == "Attendee":
-            return {
-                "emailAddress": self.emailAddress,
-                "person_id": getattr(self.person, "id", None),
-            }
+        if self.__class__.__qualname__ == "PersonData":
+            return self.id
         else:
             return nxt(self)
