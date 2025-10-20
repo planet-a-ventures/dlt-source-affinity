49b66071ce0b757a6dbab6ab20d4623b595b8581
Joscha Feth <joscha@feth.com>
2025-10-20 17:02:40 +0100

    chore: bump version to 0.1.5 and update dlt dependency to >=1.17.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dlt-source-affinity",
-    version="0.1.4",
+    version="0.1.5",
     author="Planet A GmbH",
     author_email="dev@planet-a.com",
     packages=find_packages(exclude=["tests"]),
@@ -10,7 +10,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     install_requires=[
-        "dlt>=1.5.0",
+        "dlt>=1.17.1",
         "pydantic-flatten-rootmodel>=0.1.2",
     ],
     classifiers=[
